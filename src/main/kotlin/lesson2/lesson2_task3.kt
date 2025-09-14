b61f30e2ff423820e07c7209384fed65270e1659
SFennec <spitefulfennec@gmail.com>
2025-09-14 18:12:47 +0200

    KS-2-3 Calculate train arrival time

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,22 +1,22 @@
 package org.example.lesson2
 
 const val MINUTES_IN_HOUR = 60
-const val HOURS_IN_MINUTE = 60
-const val HOURS_IN_DAYS = 24
+const val HOUR_IN_MINUTES = 60
+const val HOURS_IN_DAY = 24
 
 fun main() {
 
     val departureHours = 9
     val departureMinutes = 39
 
-    val totalMinutesOfDeparture = departureHours * HOURS_IN_MINUTE + departureMinutes
+    val totalMinutesOfDeparture = departureHours * HOUR_IN_MINUTES + departureMinutes
 
     val minutesInTrip = 457
 
     val totalMinutesArrival = totalMinutesOfDeparture + minutesInTrip
 
     val arrivalMinutes = totalMinutesArrival % MINUTES_IN_HOUR
-    val arrivalHours = totalMinutesArrival / MINUTES_IN_HOUR % HOURS_IN_DAYS
+    val arrivalHours = totalMinutesArrival / MINUTES_IN_HOUR % HOURS_IN_DAY
 
     println(String.format("%02d:%02d", arrivalHours, arrivalMinutes))
 
