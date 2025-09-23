d299608c544c9e5e43f236bbd7e8aee056be51cc
SFennec <spitefulfennec@gmail.com>
2025-09-23 15:48:04 +0200

    KS-2-1 Greetings for time of day

---
+++ src/main/kotlin/lesson3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson3
+
+fun main() {
+
+    val name = "Пользователь"
+    var greetings = "C добрым утром"
+
+    println("$greetings, $name!")
+
+    greetings = "Доброго вечера"
+
+    println("$greetings, $name!")
+
+}
