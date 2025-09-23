81c529a078339207a2886dfac466fe281eb55bc2
SFennec <spitefulfennec@gmail.com>
2025-09-23 17:31:08 +0200

    KS-3-3 Multiplication table

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,19 @@
+package org.example.lesson3
+
+fun main() {
+
+    val inputNumber = 5
+
+    println("""
+        $inputNumber x 1 = ${inputNumber}
+        $inputNumber x 2 = ${inputNumber * 2}
+        $inputNumber x 3 = ${inputNumber * 3}
+        $inputNumber x 4 = ${inputNumber * 4}
+        $inputNumber x 5 = ${inputNumber * 5}
+        $inputNumber x 6 = ${inputNumber * 6}
+        $inputNumber x 7 = ${inputNumber * 7}
+        $inputNumber x 8 = ${inputNumber * 8}
+        $inputNumber x 9 = ${inputNumber * 9}
+    """.trimIndent())
+
+}
