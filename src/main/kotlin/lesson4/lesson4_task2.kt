97d14ee93848abe22b212e4a752c773d99293d97
SFennec <spitefulfennec@gmail.com>
2025-09-24 17:37:57 +0200

    KS-4-2 Average cargo weight

---
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson4
+
+const val MIN_CARGO_WEIGHT = 35
+const val MAX_CARGO_WEIGHT = 100
+const val MAX_CARGO_VOLUME = 100
+
+fun main() {
+
+    var cargoWeight = 20
+    var cargoVolume = 80
+
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight > MIN_CARGO_WEIGHT && cargoWeight <= MAX_CARGO_WEIGHT && cargoVolume < MAX_CARGO_VOLUME}")
+
+    cargoWeight = 50
+    cargoVolume = 100
+
+    println("Груз с весом $cargoWeight кг и объемом $cargoVolume л соответствует категории 'Average': ${cargoWeight > MIN_CARGO_WEIGHT && cargoWeight <= MAX_CARGO_WEIGHT && cargoVolume < MAX_CARGO_VOLUME}")
+
+}
